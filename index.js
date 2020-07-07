f59f05eeb00f519ad07588b701607760eb015b4f
IzPOD <userru@ya.ru>
2020-07-07 21:45:48 +0300

    qwe

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = process.env.TOKEN;
+//const token = process.env.TOKEN;
+token = "NzMwMDE0ODc2OTgyMDUwODc2.XwSP5w.kWLXOt2la8CSQb4B5DErg_BcBNI";
 
 const exampleEmbed = new Discord.MessageEmbed().setImage('https://unvegetariano.com/images/sad-cat-png-3.png');
 var logChannel;
@@ -42,10 +43,18 @@ bot.on('message', msg=>{
 // });
 
 bot.on('message', msg=>{
-	if(msg.content == "embed") {
+	if(msg.content == "embed" ) {
     msg.channel.send(exampleEmbed);
 	}
 });
+
+
+
+bot.on ('message', msg=>{
+	if(msg.content == "онлайн" && msg.author.bot == true) {
+    msg.delete({timeout: 2000});
+	}
+})
 
 bot.on('message', msg=> {
   var splitted = msg.content.split(' ');
