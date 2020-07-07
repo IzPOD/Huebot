45475e4f0c4cfbd16f1922dc09aa5e0a0e5dd892
izpod <userru@ya.ru>
2020-07-07 15:29:53 +0300

    second

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = 'NzMwMDE0ODc2OTgyMDUwODc2.XwRVfA.M1_ULpAemPy48Ea25kpUyLanngA';
+const token = process.env.TOKEN;
 
 bot.on('ready', () =>{
 	console.log('bot online');
 })
+
+bot.on('message', msg=>){
+	if(msg.content == "HELLO"){
+		msg.reply('SOSI HUI DIMA');
+	}
+}
 
 bot.login(token);
