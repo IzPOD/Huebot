cf8300d56f8a8b342deee300b20955eb3494950f
IzPOD <userru@ya.ru>
2020-07-07 16:51:14 +0300

    VSE SUKA

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = process.env.TOKEN;
 
+
 bot.on('ready', () =>{
 	console.log('bot online');
 })
 
-bot.on('message', msg=>){
+bot.on('message', msg=>{
 	if(msg.content == "HELLO"){
 		msg.reply('SOSI HUI DIMA');
 	}
-}
+})
 
 bot.login(token);
