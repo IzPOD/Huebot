89631fd097232e30527b905b9884f1fae44af55f
Marchenko Dmitry Igorevich <Foloke@yandex.ru>
2020-07-07 17:00:33 +0300

    huebot 1.0

--- index.js
+++ index.js
@@ -2,15 +2,65 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = process.env.TOKEN;
 
+const exampleEmbed = new Discord.MessageEmbed().setImage('https://unvegetariano.com/images/sad-cat-png-3.png');
+var logChannel;
 
-bot.on('ready', () =>{
+bot.on('ready', () => {
 	console.log('bot online');
-})
+	bot.guilds.cache.forEach(server => {
+		server.channels.cache.forEach(channel => {
+			if(channel.name == "лог") {
+				channel.send('я онлайн');
+				logChannel = channel;
+			}
+			//channel.send('я живой');
+		});
+	});
+	
+	bot.user.setActivity(`this won't appear in the bot's custom status!`, {type: 4});
+});
+
+bot.on('userUpdate', (oldUser, newUser) => {
+	logChannel.send(newUser);
+});
 
 bot.on('message', msg=>{
 	if(msg.content == "HELLO"){
-		msg.reply('SOSI HUI DIMA');
+		msg.reply('SOSI HUI ' + msg.author.username);
 	}
-})
+});
+
+bot.on('message', msg=>{
+	if(msg.content == "Хай" || msg.content == "хай"){
+    msg.channel.send(msg.author.username + ' соси хуй');
+	}
+});
+
+bot.on("guildMemberAdd", member => {
+    member.send(exampleEmbed)
+        .catch(console.error);
+});
+
+bot.on('message', msg=>{
+	if(msg.content == "embed") {
+    msg.channel.send(exampleEmbed);
+	}
+});
+
+bot.on('message', msg=> {
+  var splitted = msg.content.split(' ');
+  if(splitted.length > 1) {
+    if((splitted[0] == ("roll")) || (splitted[0] == ("ролл"))) {
+      var number = Math.floor(Math.random() * splitted[1] + 1);
+      var response = '' + number;
+      if(number == 1) {
+        response += ' (Критическая неудача)';
+      } else if (number == splitted[1]) {
+        response += ' (Critical damage)';
+      }
+       msg.reply(response);
+    }
+  }
+});
 
 bot.login(token);
